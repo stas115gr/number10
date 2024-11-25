191fadeae92a665a9de68dd5c10a872aeb5b9992
stas115gr <stashauzkin10@gmail.com>
2024-11-25 19:22:06 +0200

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// вычисление
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// точка ввода в программу
+        /// </summary>
+        /// <param name="args">строковый массив</param>
+        static void Main(string[] args)
+        {
+            double y = Fune(5) + Fune(12) + Fune(19);
+            Console.WriteLine($"y={y}");
+            Console.ReadKey();
+        }
+        public static double Fune(int x)
+        {
+            return (Math.Sqrt(x) + x / 2);
+        }
+    }
+}
